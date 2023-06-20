b1ecefff8d47d71ccea287b16d3d54924e2310f3
jenna5376 <jenna5376@gmail.com>
2023-06-20 12:59:47 +0900

    sheetsdb test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  fetch('https://sheetdb.io/api/v1/vv1l83xybuwf3')
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
