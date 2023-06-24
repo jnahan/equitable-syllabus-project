9cfed5081309b38d9489c5a75c2682e97916aa3b
jenna5376 <jenna5376@gmail.com>
2023-06-24 19:19:10 +0900

    markdown import test

--- src/pages/SyllabusGuide/SyllabusGuide.jsx
+++ src/pages/SyllabusGuide/SyllabusGuide.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { useEffect, useState } from "react"
+import Markdown from 'markdown-to-jsx'
 
 function SyllabusGuide() {
+  const [postContent, setPostContent] = useState("");
+  const fileName = "markdown.md"
+
+  useEffect(() => {
+    import(`../../markdown/${fileName}`)
+      .then(res => {
+        fetch(res.default)
+          .then(response => response.text())
+          .then(response => setPostContent(response))
+          .catch(err => console.log(err))
+      })
+  }, []);
+
   return (
-    <div>
+    <div className='section'>
+      <Markdown className='md'>{postContent}</Markdown>
     </div>
   )
 }
