0987d4d0ee283c9478eeb7deddcb37fa71ed6928
jenna5376 <jenna5376@gmail.com>
2023-09-20 01:20:28 -0400

    added bianca to team page

--- src/pages/Team/Team.jsx
+++ src/pages/Team/Team.jsx
@@ -7,6 +7,7 @@ import DivyaAvatar from "../../images/team/divya-avatar.png"
 import JennaAvatar from "../../images/team/jenna-avatar.png"
 import KayleyAvatar from "../../images/team/kayley-avatar.png"
 import SophiaAvatar from "../../images/team/sophia-avatar.png"
+import BiancaAvatar from "../../images/team/bianca-avatar.png"
 
 import Blair from "../../images/team/blair.png"
 import Chaski from "../../images/team/chaski.png"
@@ -14,6 +15,7 @@ import Divya from "../../images/team/divya.png"
 import Jenna from "../../images/team/jenna.png"
 import Kayley from "../../images/team/kayley.png"
 import Sophia from "../../images/team/sophia.png"
+import Bianca from "../../images/team/bianca.png"
 
 function Team() {
   return (
@@ -28,23 +30,24 @@ function Team() {
           <img className="team__avatar" src={JennaAvatar} alt="Jenna Han" />
           <img className="team__avatar" src={ChaskiAvatar} alt="Chaski No" />
           <img className="team__avatar" src={DivyaAvatar} alt="Divya Mehra" />
+          <img className="team__avatar" src={BiancaAvatar} alt="Divya Mehra" />
         </div>
       </div>
       <div className="section">
         <h2 className="section__title text-center">Current Members</h2>
         <div className="team__card-wrapper">
-        <TeamCard image = {Blair} name = "Bianca Gan" role = "Resident Specialist" email = "blair.simmons@nyu.edu"/>
           <TeamCard image = {Blair} name = "Blair Simmons" role = "Project Manager" email = "blair.simmons@nyu.edu"/>
           <TeamCard image = {Jenna} name = "Jenna Han" role = "Web Designer" email = "jennahan@nyu.edu"/>
           <TeamCard image = {Kayley} name = "Kayley Chery" role = "Research Specialist" email = "kac7748@nyu.edu "/>
           <TeamCard image = {Sophia} name = "Sophia Edwards" role = "Student Lead" email = "sge7432@nyu.edu"/>
+          <TeamCard image = {Chaski} name = "Chaski No" role = "Technical Project Manager" email = "chaski@nyu.edu"/>
+          <TeamCard image = {Divya} name = "Divya Mehra" role = "Researcher" email = "dm4924@nyu.edu"/>
+          <TeamCard image = {Bianca} name = "Bianca Gan" role = "Researcher" email = "bag5392@nyu.edu"/>
         </div>
       </div>
       <div className="section">
         <h2 className="section__title text-center">Alumni</h2>
         <div className="team__card-wrapper">
-          <TeamCard image = {Chaski} name = "Chaski No" role = "Technical Project Manager" email = "chaski@nyu.edu"/>
-          <TeamCard image = {Divya} name = "Divya Mehra" role = "Researcher" email = "dm4924@nyu.edu"/>
           <TeamCard name = "Rashida Kamal" role = "Technical Project Manager" />
           <TeamCard name = "Spencer Ratanavanh" role = "Researcher" />
           <TeamCard name = "Polina Kolesnikova" role = "Researcher" />
